7d7020ffd9142295675e8d08aaa91b5b271d307f
Jan Hildebrandt <j-hdt@web.de>
2015-09-07 16:29:55 +0200

    Initial commit

---
+++ app/src/main/java/luh/energiesparen/MainActivity.java
@@ -0,0 +1,155 @@
+package luh.energiesparen;
+
+import android.content.res.Configuration;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+    private NavigationView mNavigationView;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private FragmentManager mFragmentManager;
+    private FragmentTransaction mFragmentTransaction;
+    private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
+    private int mCurrentSelectedPosition;
+    private boolean mFromSavedInstanceState;
+    private String[] titles;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.nvView);
+
+        mActionBarDrawerToggle = setupDrawerToggle();
+
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+        mFragmentManager = getSupportFragmentManager();
+
+        titles = getResources().getStringArray(R.array.navTitles);
+
+        mNavigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        if(menuItem.isCheckable()) {
+                            menuItem.setChecked(true);
+                        }
+                        if(menuItem.isChecked()) {
+                            setTitle(menuItem.getTitle());
+                        }
+                        mDrawerLayout.closeDrawers();
+
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_item_01_home:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.flContent, new HomeFragment()).commit();
+                                mCurrentSelectedPosition = 0;
+                                break;
+                            case R.id.nav_item_02_etc:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.flContent, new BlankFragment()).commit();
+                                mCurrentSelectedPosition = 1;
+                                break;
+                            case R.id.nav_item_03_tab1:
+                                //getSupportFragmentManager().beginTransaction().replace(R.id.flContent, TabsFragment.newInstance(getResources().getStringArray(R.array.tabtitles1))).commit();
+                                getSupportFragmentManager().beginTransaction().replace(R.id.flContent, TabsFragment.newInstance(getResources().getStringArray(R.array.tabtitles1),
+                                        getResources().getStringArray(R.array.STROMBAD), getResources().getStringArray(R.array.STROMKUECHE), getResources().getStringArray(R.array.STROMwohnung))).commit();
+                                break;
+                            case R.id.nav_item_04_tab2:
+                                 getSupportFragmentManager().beginTransaction().replace(R.id.flContent, new TabsFragment()).commit();
+                                break;
+                        }
+                        return false;
+                    }
+                });
+        if (savedInstanceState != null) {
+            mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
+            mFromSavedInstanceState = true;
+            setTitle(titles[mCurrentSelectedPosition]);
+        } else {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flContent, new HomeFragment()).commit();
+            setTitle(titles[0]);
+
+        }
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(findViewById(R.id.nvView))) {
+            mDrawerLayout.closeDrawer(findViewById(R.id.nvView));
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        } else if (id == R.id.action_settings) {
+            Toast.makeText(MainActivity.this, "settings pressed", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mActionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(STATE_SELECTED_POSITION, mCurrentSelectedPosition);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION, 0);
+        Menu menu = mNavigationView.getMenu();
+        menu.getItem(mCurrentSelectedPosition).setChecked(true);
+    }
+}
---
+++ app/src/main/java/luh/energiesparen/TabsFragment.java
@@ -0,0 +1,122 @@
+package luh.energiesparen;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabsFragment extends Fragment {
+
+    private static String TAB_TITLES = "tab_titles";
+    private static String TAB_LISTE1 = "tab_liste1";
+    private static String TAB_LISTE2 = "tab_liste2";
+    private static String TAB_LISTE3 = "tab_liste3";
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public int int_items = 3;
+    public String[] tabstitles;
+    public String[] liste1;
+    public String[] liste2;
+    public String[] liste3;
+
+
+    public TabsFragment() {
+        // Required empty public constructor
+    }
+
+    public static TabsFragment newInstance(String[] titles,
+                                           String[] liste1,
+                                           String[] liste2,
+                                           String[] liste3) {
+        Bundle args = new Bundle();
+        args.putStringArray(TAB_TITLES, titles);
+        args.putStringArray(TAB_LISTE1, liste1);
+        args.putStringArray(TAB_LISTE2, liste2);
+        args.putStringArray(TAB_LISTE3, liste3);
+        TabsFragment fragment = new TabsFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        tabstitles = getArguments().getStringArray(TAB_TITLES);
+        liste1 = getArguments().getStringArray(TAB_LISTE1);
+        liste2 = getArguments().getStringArray(TAB_LISTE2);
+        liste3 = getArguments().getStringArray(TAB_LISTE3);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_tabs, container, false);
+
+        tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+
+
+
+        tabstitles = getActivity().getResources().getStringArray(R.array.tabtitles1);
+        int_items = tabstitles.length;
+
+        viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+            }
+        });
+
+        return view;
+    }
+
+    class MyAdapter extends FragmentPagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+/*            String[] inhalte = {"etc19", "etc29", "cte39"};
+            return TabListenFragment.newInstance(inhalte);*/
+
+            switch (position) {
+                case 0:
+                    return TabListenFragment.newInstance(liste1);
+                case 1:
+                    return TabListenFragment.newInstance(liste2);
+                case 2:
+                    return TabListenFragment.newInstance(liste3);
+
+            }
+            return new TabListenFragment();
+        }
+
+
+        @Override
+        public int getCount() {
+            return int_items;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabstitles[position];
+        }
+    }
+
+}
