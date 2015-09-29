7f8898ddeb2d1256f1a159447717fa46a2cf91c4
Jan Hildebrandt <j-hdt@web.de>
2015-09-29 09:47:39 +0200

    Impressum & Rechtliches updated

--- app/src/main/java/luh/energiesparen/ImpressumActivity.java
+++ app/src/main/java/luh/energiesparen/ImpressumActivity.java
@@ -4,11 +4,13 @@ package luh.energiesparen;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.widget.TextView;
 
 
 public class ImpressumActivity extends AppCompatActivity {
     private Toolbar mToolbar;
-
+    private TextView impressum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,26 @@ public class ImpressumActivity extends AppCompatActivity {
                 getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             }
         }
+
+        impressum = (TextView) findViewById(R.id.textView_impressum01);
+        impressum.setText(Html.fromHtml("<h1>Impressum</h1>" +
+                "<h2>Angaben gemäß § 5 TMG:</h2>" +
+                "<p>Max Mustermann<br />" +
+                "Mega Musterladen<br />" +
+                "Musterstraße 111<br />" +
+                "Gebäude 44<br />" +
+                "90210 Musterstadt" +
+                "</p>" +
+                "<h2>Kontakt:</h2>" +
+                "Telefon: +49 (0) 123 44 55 66<br/>" +
+                "Telefax:+49 (0) 123 44 55 99<br/>" +
+                "E-Mail: mustermann@musterfirma.de<br/></p>" +
+                "<h2>Umsatzsteuer-ID:</h2>" +
+                "<p>Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:<br />" +
+                "DE 999 999 999</p>" +
+                "<h2>Quellenangaben für die verwendeten Bilder und Grafiken:</h2>" +
+                "<div>Grafiken erstellt durch <a href=\"http://www.freepik.com\" title=\"Freepik\">Freepik</a> von <a href=\"http://www.flaticon.\" title=\"Flaticon\">www.flaticon.com</a> sind lizensiert unter <a href=\"http://creativecommons.org/licenses/by/3.0/\" title=\"Creative Commons BY 3.0\">CC BY 3.0</a></div>" +
+                "<p> </p>" +
+                "<p>Quelle: <em><a rel=\"nofollow\" href=\"http://www.e-recht24.de/impressum-generator.html\">http://www.e-recht24.de</a></em></p>\n"));
     }
 }
--- app/src/main/java/luh/energiesparen/RechtlichesActivity.java
+++ app/src/main/java/luh/energiesparen/RechtlichesActivity.java
@@ -3,11 +3,14 @@ package luh.energiesparen;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
+import android.text.Html;
+import android.widget.TextView;
 
 
 public class RechtlichesActivity extends AppCompatActivity {
     private Toolbar mToolbar;
+    private TextView haftungsausschluss;
+    private TextView haftungicons;
 
 
     @Override
@@ -23,6 +26,39 @@ public class RechtlichesActivity extends AppCompatActivity {
                 getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             }
         }
+
+        haftungsausschluss = (TextView) findViewById(R.id.textView_haftung01);
+        haftungsausschluss.setText(Html.fromHtml("<h1>Haftungsausschluss (Disclaimer)</h1><p><strong>Haftung für Inhalte</strong></p>" +
+                        " <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte" +
+                        " auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8" +
+                        " bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte" +
+                        " oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen" +
+                        ", die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder" +
+                        " Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon" +
+                        " unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis" +
+                        " einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen" +
+                        " werden wir diese Inhalte umgehend entfernen.</p> <p><strong>Haftung für Links" +
+                        "</strong></p> <p>Unser Angebot enthält Links zu externen Webseiten Dritter, auf" +
+                        " deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte" +
+                        " auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige" +
+                        " Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt" +
+                        " der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt" +
+                        " der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist" +
+                        " jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von" +
+                        " Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p> <p><strong>Urheberrecht" +
+                        "</strong></p> <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten " +
+                        "unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede " +
+                        "Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung" +
+                        " des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten," +
+                        " nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber" +
+                        " erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter " +
+                        "als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden" +
+                        ", bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir " +
+                        "derartige Inhalte umgehend entfernen.</p><p>" +
+                        "<strong> Ende Haftungsausschluss </strong> </p>"));
+
+        haftungicons = (TextView) findViewById(R.id.textView_haftung02);
+        haftungicons.setText(Html.fromHtml("<div>Icons made by <a href=\"http://www.freepik.com\" title=\"Freepik\">Freepik</a> from <a href=\"http://www.flaticon.com\" title=\"Flaticon\">www.flaticon.com</a> is licensed under <a href=\"http://creativecommons.org/licenses/by/3.0/\" title=\"Creative Commons BY 3.0\">CC BY 3.0</a></div>"));
 
     }
 }
